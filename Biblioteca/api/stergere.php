7bb148941cd68fdeb9f8abaf0d29f80815edf508
Bordea Cosmin <bordea37@gmail.com>
2021-05-01 02:28:29 +0300

    no message

--- Biblioteca/api/stergere.php
+++ Biblioteca/api/stergere.php
@@ -2,6 +2,8 @@
 require 'SQL.php';
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $_POST = json_decode(file_get_contents('php://input'), true);
+    $sql = "DELETE FROM monitor WHERE ID_carte=".$_POST['ID'].";";
+    $result = mysqli_query($conn, $sql);
     $sql = "DELETE FROM carti WHERE ID=".$_POST['ID'].";";
     $result = mysqli_query($conn, $sql);
 }
